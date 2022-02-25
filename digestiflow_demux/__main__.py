23c3abd0c22a33084d02ed222277d508694c668d
Clemens Messerschmidt <clemens.messerschmidt@bihealth.de>
2022-02-25 10:27:11 +0100

    Fix flake8errors, where possible and add noqa where not.

--- digestiflow_demux/__main__.py
+++ digestiflow_demux/__main__.py
@@ -13,7 +13,6 @@ import toml
 
 from .api_client import ApiClient
 from .exceptions import ApiProblemException
-from .bases_mask import BaseMaskConfigException
 from .workflow import perform_demultiplexing
 
 from ._version import get_versions
--- digestiflow_demux/wrappers/multiqc/wrapper.py
+++ digestiflow_demux/wrappers/multiqc/wrapper.py
@@ -12,7 +12,7 @@ __author__ = "Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>"
 shell.executable("/bin/bash")
 
 with tempfile.NamedTemporaryFile("wt+") as listf:
-    print("\n".join(snakemake.input), file=listf)
+    print("\n".join(snakemake.input), file=listf)  # noqa
     listf.flush()
     listf.seek(0)
 
